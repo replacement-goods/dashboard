7a326c5e5729898927796a678c5b35b6b3bd4121
Andy Gillette <andy.gillette@gmail.com>
2019-09-16 14:08:56 -0700

    add message for no referrals yet

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,31 +16,36 @@ class IndexComponent extends React.Component {
     cleanData.map((row) => {
       total += Number(row.node.Amount.replace(/[^0-9.-]+/g,""));
     });
+    const hasNoReferrals = total === 0;
+    console.log('has no referfals', hasNoReferrals);
     return (
       <div style={{ maxWidth: '1024px', width: 'calc(100% - 50px)', margin: '0 auto' }}>
-        <p>You've earned {`$${total.toFixed(2)}`} to date, worth {total / 2} shares.</p>
-        <table>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Referrer</th>
-              <th>Name</th>
-              <th>Amount</th>
-              <th>Shares Earned</th>
-            </tr>
-          </thead>
-          <tbody>
-            {cleanData.map((row, i) => (
-              <tr key={`${row.node.Pledge_ID} ${i}`}>
-                <td>{row.node.Pledge_ID}</td>
-                <td>{row.node.Referrer_ID}</td>
-                <td>{row.node.Name}</td>
-                <td>{row.node.Amount}</td>
-                <td>{Number(row.node.Amount.replace(/[^0-9.-]+/g,"")) / 2}</td>
+        <p style={{ margin: '1em 0' }}>You've earned {`$${total.toFixed(2)}`} to date, worth {total / 2} shares.</p>
+        {hasNoReferrals
+          ? <p>No referrals yet!</p>
+          : <table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Referrer</th>
+                <th>Name</th>
+                <th>Amount</th>
+                <th>Shares Earned</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {cleanData.map((row, i) => (
+                <tr key={`${row.node.Pledge_ID} ${i}`}>
+                  <td>{row.node.Pledge_ID}</td>
+                  <td>{row.node.Referrer_ID}</td>
+                  <td>{row.node.Name}</td>
+                  <td>{row.node.Amount}</td>
+                  <td>{Number(row.node.Amount.replace(/[^0-9.-]+/g,"")) / 2}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        }
       </div>
     )
   }
